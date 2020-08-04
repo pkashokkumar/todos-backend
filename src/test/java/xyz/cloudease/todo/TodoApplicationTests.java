c2825dcd0b93e174a72812ac283b8d8ab727edea
Ashok Kumar <kumarashok@vmware.com>
2020-08-03 23:11:35 -0700

    Initial commit

---
+++ src/test/java/xyz/cloudease/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.cloudease.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
